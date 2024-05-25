aaa1536be60139b67b484938a63d94749277ffec
decastroJR <165857513+decastroJR@users.noreply.github.com>
2024-05-24 23:05:10 -0300

    Add files via upload
    javascript aps 2 prova

---
+++ funçao 10 maiores numeros primos.js
@@ -0,0 +1,34 @@
+function ehPrime(num) {
+  if (num <= 1) {
+      return false;
+  }
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i === 0) {
+          return false;
+      }
+  }
+  return true;
+}
+
+function findLargestPrimes() {
+  const userInput = parseInt(prompt("Digite um número:"));
+  if (isNaN(userInput)) {
+      console.log("Entrada inválida. Por favor, insira um número válido.");
+      return;
+  }
+
+  const largestPrimes = [];
+  let currentNumber = userInput;
+  while (largestPrimes.length < 10) {
+      if (ehPrime(currentNumber)) {
+          largestPrimes.push(currentNumber);
+      }
+      currentNumber--;
+  }
+
+  console.log("Os 10 maiores números primos a partir de", userInput, "são:");
+  console.log(largestPrimes);
+}
+
+// Chame a função para encontrar os 10 maiores números primos
+findLargestPrimes();
